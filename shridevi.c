30becf10d0218c8be92e5681e6490f83af7d8199
kavya381 <prakashkumarikavya17@gmail.com>
2025-05-13 15:43:43 +0530

    Create shridevi.c

---
+++ shridevi.c
@@ -0,0 +1,13 @@
+//function declaration and //function calling
+#include<stdio.h>
+int shridevi(){
+printf("kavya\n");
+printf("d-o-b\n");
+printf("age\n");
+printf("mobile number\n");
+}
+int main()
+{
+shridevi();
+return 0;
+}
