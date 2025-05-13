6a866cbfc4bed070d4ace7e1969f91f2b9f8963e
kavya381 <prakashkumarikavya17@gmail.com>
2025-05-13 16:26:24 +0530

    largest.c

---
+++ largest.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2;
+printf("enter the two differnt values");
+scanf("%d%d",&n1,&n2);
+if(n1>n2)
+{
+printf("%d is largest",n1);
+}
+else if(n2>n1)
+{
+printf("%d is largest",n2);
+}
+}
