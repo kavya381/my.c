83798da7b131a94d9a1f6c8193d336d7cb608960
kavya381 <prakashkumarikavya17@gmail.com>
2025-05-13 15:24:47 +0530

    Create function call.c

---
+++ function call.c
@@ -0,0 +1,13 @@
+//function declaration and //function calling
+#include<stdio.h>
+int c;
+int add(int a, int b){
+return a+b;
+}
+int main()
+//function calling
+{
+int result=add(6,9);
+printf("sum=%d\n",result);
+return 0;
+}
