942ea37a01369baaf9e6aec8df7d7e80d6a3782d
kavya381 <prakashkumarikavya17@gmail.com>
2025-05-15 15:35:58 +0530

    Update Name_age_location.c

--- Name_age_location.c
+++ Name_age_location.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-int main() 
-char name[] = "Poorvi";
+int main() {
+char name[] = "kavya";
 int age = 18;
-char location[] = "Mysore, Bengalur";
+char location[] = "Mysore";
 printf("Name: %s\n", name);
 printf("Age: %d\n", age);
 printf("Location: %s\n", location);
 return 0;
+}
+o/p 
+Name kavya
+Age 18
+Location Mysore
+o/p
+Name kavya
+Age 18
+Location Mysore
+
+
